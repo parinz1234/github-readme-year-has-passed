7891d6ac50c7a4057a753bb83ac1a4b7a50450e9
parinz1234 <parinya.ons@gmail.com>
2022-02-03 22:12:12 +0700

    feat: test new svg 1

--- api/index.go
+++ api/index.go
@@ -13,7 +13,14 @@ type data struct {
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 
-	const tpl = `{{.Top}}<svg width="640" height="480" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg"><g><title>Layer 1</title><rect id="svg_1" height="126" width="165" y="109" x="155" stroke-width="5" stroke="#000000" fill="{{.Color}}"/></g></svg>
+	const tpl = `<!doctype html>
+	<svg viewBox="0 0 400 130" width="400" height="130">
+		<rect width="380" height="80" style="stroke: rgb(0, 0, 0); fill: rgb(51, 51, 51); fill-rule: nonzero;" data-bx-origin="-0.038 -0.658333" x="20" y="50"></rect>
+		<text style="white-space: pre; fill: rgb(51, 51, 51); font-family: Arial, sans-serif; font-size: 17.2px;" x="258.918" y="49.952"> </text>
+		<g></g>
+		<text style="fill: rgb(51, 51, 51); font-family: Arial, sans-serif; font-size: 19.2px; white-space: pre;" data-bx-origin="-0.076015 -0.947915" transform="matrix(1.245747, 0, 0, 0.950186, -4.914937, 2.73884)"><tspan x="20" y="35.375" style="text-decoration: underline solid rgba(255, 255, 255, 0.9);">6% of 2022 has passed</tspan></text>
+		<rect x="30" y="60" width="360" height="60" style="stroke: rgb(0, 0, 0); fill: rgb(0, 253, 49);"></rect>
+	</svg>
 	`
 
 	check := func(err error) {
