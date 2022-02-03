0345a781f419c2bdc1e91e42f3792920410f3bd7
parinz1234 <parinya.ons@gmail.com>
2022-02-03 21:24:33 +0700

    feat: test example svg

--- api/index.go
+++ api/index.go
@@ -6,38 +6,36 @@ import (
 	"net/http"
 )
 
+type data struct {
+	Top   template.HTML
+	Color string
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
 
 	const tpl = `
-<?xml version=\"1.0\" encoding=\"utf-8\" ?>
-<svg width="640" height="480" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
-	<!-- Created with SVG-edit - http://svg-edit.googlecode.com/ -->
-	<g>
-	<title>Layer 1</title>
-	<rect id="svg_1" height="126" width="165" y="109" x="155" stroke-width="5" stroke="#000000" fill="#ff00ff"/>
-	</g>
-</svg>
- `
+	{{.Top}}
+	<svg width="640" height="480" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
+	 <!-- Created with SVG-edit - http://svg-edit.googlecode.com/ -->
+	 <g>
+		<title>Layer 1</title>
+		<rect id="svg_1" height="126" width="165" y="109" x="155" stroke-width="5" stroke="#000000" fill="{{.Color}}"/>
+	 </g>
+	</svg>
+	`
 
 	check := func(err error) {
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
+
 	t, err := template.New("svg").Parse(tpl)
 	check(err)
 
-	type Data struct {
-		Title       string
-		Description string
-		Percentage  int8
-	}
-
-	data := Data{
-		Title:      "6% of 2022 has passed",
-		Percentage: 10,
-	}
-
-	err = t.Execute(w, data)
+	var d data
+	d.Top = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>"
+	d.Color = "#ff00ff"
+	err = t.ExecuteTemplate(w, "svg", &d)
 	check(err)
 }
